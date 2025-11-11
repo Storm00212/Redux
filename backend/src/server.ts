4c7b812d993bc4a3cfebaa4f25257451d8aaa4ea
Storm00212 <paulmuyalikhams@gmail.com>
2025-11-11 15:20:41 +0300

    fully updated server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import './db/dbconfig'; // Initialize database connection
 import pool from './db/dbconfig';
-
+import userRoutes from './routes/user.routes';
+import todoRoutes from './routes/todo.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/todos', todoRoutes);
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from backend!' });
@@ -23,6 +29,7 @@ app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
   } catch (error) {
     console.log("Error connecting to the database or server error.", error)
+
   }
-  
+
 });
