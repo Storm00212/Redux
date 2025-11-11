438c9ee03ffeb95e1e025205c9dc449f7f3f91b1
Storm00212 <paulmuyalikhams@gmail.com>
2025-11-11 16:20:02 +0300

    comments in todo schema

--- backend/src/types/todo.schema.ts
+++ backend/src/types/todo.schema.ts
@@ -1,3 +1,12 @@
+/**
+ * Todo-related TypeScript type definitions.
+ * Defines the structure of todo data used throughout the application.
+ */
+
+/**
+ * Represents a todo item entity from the database.
+ * Contains todo information including creator reference via user ID.
+ */
 export type todo = {
     id: number,
     name: string,
