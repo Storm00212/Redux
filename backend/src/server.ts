900ca86d8476cb14b997eb95755ef8485be4803e
Storm00212 <paulmuyalikhams@gmail.com>
2025-11-11 12:21:02 +0300

    added database connection to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import './db/dbconfig'; // Initialize database connection
+import pool from './db/dbconfig';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,17 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    const dbconnect = await pool.connect()
+    if(dbconnect){
+      console.log("Database connected successfully✅")
+    } else{
+      console.log("Database connection error ❌")
+    }
+    console.log(`Server running on port ${PORT}`);
+  } catch (error) {
+    console.log("Error connecting to the database or server error.", error)
+  }
+  
 });
