bbc0db3d031ed22631df5762ff9c8a860c33ff58
Storm00212 <paulmuyalikhams@gmail.com>
2025-11-11 20:31:55 +0300

    index.ts file

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Redux Store Configuration
+ * Main store setup for the Redux tutorial application.
+ * Combines user and todo slices, configures middleware, and exports types.
+ *
+ * TODO: Implement the following:
+ * - Import configureStore from @reduxjs/toolkit
+ * - Import user and todo slices
+ * - Configure the store with reducer and middleware
+ * - Export RootState and AppDispatch types
+ * - Export the store instance
+ */
+
+// TODO: Import necessary Redux Toolkit functions
+// import { configureStore } from '@reduxjs/toolkit';
+
+// TODO: Import slices
+// import userSlice from './slices/userSlice';
+// import todoSlice from './slices/todoSlice';
+
+// TODO: Configure the Redux store
+// export const store = configureStore({ ... });
+
+// TODO: Export types for TypeScript
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+// TODO: Export store
+// export default store;
