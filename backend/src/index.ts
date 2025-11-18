810f387094fa1213d0a2abd1c65222c6cb8cfe39
Storm00212 <paulmuyalikhams@gmail.com>
2025-11-18 16:38:06 +0300

    successful server setup and database connection.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,34 @@
 import express, { Express } from "express";
+import { config } from "dotenv";
+import { getPool } from "./db/config";
 
-
-
-
+config();
+const PORT = process.env.PORT || 3000
 
 const app: Express = express();
 
-app.listen
+app.listen(PORT, async() => {
+     try {
+        const dbconnect = await getPool();
+        if (dbconnect){
+            console.log("Database connected successfully")
+            console.log("Server running on http://localhost:3000")
+        }
+     } catch (error) {
+       console.log("Error starting the server", error); 
+     }
+})
+
+app.get('/', (req, res) =>{
+    res.json({
+        message:" Server is up and running",
+        version: "1.0.0",
+        endpoints: {
+            auth: "/auth",
+            todo: "/todo",
+            user: "/user"
+        }
+    })
+})
 
 export default app;
